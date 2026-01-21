dc54b3e1a0c41c275639facaf5ea4cc31db366c0
Prasad <prasad25patil2005@gmail.com>
2026-01-21 17:06:00 +0530

    Start Server Before Even Loading 2

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -103,7 +103,9 @@ export const AuthProvider = ({ children }) => {
   const startServer = async () => {
     try {
       await toast.promise(
-        await axios.get("/api/status"),
+        async () => {
+          await axios.get("/api/status")
+        },
         {
           loading: "Starting Server...",
           success: "Server Started",
