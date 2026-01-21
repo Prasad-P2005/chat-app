d8467898be3002e2d9c5c8c2203e376c43f6a223
Prasad <prasad25patil2005@gmail.com>
2026-01-21 16:12:14 +0530

    Final Touch 1

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center
-  justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+  justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-3xl'>
 
       {/* left logo */}
       <img src={assets.logo_big} alt="" className='w-[min(30vw, 250px)]' />
