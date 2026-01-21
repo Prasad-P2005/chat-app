35eb26a855c151a9bec89dfe78b989f3dc892106
Prasad <prasad25patil2005@gmail.com>
2026-01-21 14:35:07 +0530

    Added Token Expiry Of 1 min

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken"
 
 export const generateToken = (userId) => {
-  const token = jwt.sign({userId}, process.env.JWT_SECRET)
+  const token = jwt.sign({
+    exp: Math.floor(Date.now() / 1000) + (60),
+    data: userId,
+  }, process.env.JWT_SECRET)
   return token
 }
